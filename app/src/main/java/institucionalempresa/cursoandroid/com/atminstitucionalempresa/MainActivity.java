6dd434cbd02b9405d76cfddffabbe5958e44b756
Leonardo Webster <leonardowebster15@gmail.com>
2018-06-19 09:55:39 -0300

    Pagina de Contato

--- app/src/main/java/institucionalempresa/cursoandroid/com/atminstitucionalempresa/MainActivity.java
+++ app/src/main/java/institucionalempresa/cursoandroid/com/atminstitucionalempresa/MainActivity.java
@@ -44,5 +44,12 @@ public class MainActivity extends Activity {
                 startActivity(new Intent(MainActivity.this, SobreClientesActivity.class));
             }
         });
+
+        sobreContato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SobreContatoActivity.class));
+            }
+        });
     }
 }

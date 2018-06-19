26a9c150e6769b7bbe36e252cff5e56563567263
Leonardo Webster <leonardowebster15@gmail.com>
2018-06-19 09:17:56 -0300

    Adicionando view sobre a empresa

--- app/src/main/java/institucionalempresa/cursoandroid/com/atminstitucionalempresa/MainActivity.java
+++ app/src/main/java/institucionalempresa/cursoandroid/com/atminstitucionalempresa/MainActivity.java
@@ -1,14 +1,34 @@
 package institucionalempresa.cursoandroid.com.atminstitucionalempresa;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+    private ImageView sobreEmpresa;
+    private ImageView sobreServicos;
+    private ImageView sobreClientes;
+    private ImageView sobreContato;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sobreEmpresa  = findViewById(R.id.sobreEmpresa);
+        sobreServicos = findViewById(R.id.sobreServicos);
+        sobreClientes = findViewById(R.id.sobreClientes);
+        sobreContato  = findViewById(R.id.sobreContato);
+
+        sobreEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SobreEmpresaActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/institucionalempresa/cursoandroid/com/atminstitucionalempresa/SobreEmpresaActivity.java
@@ -0,0 +1,13 @@
+package institucionalempresa.cursoandroid.com.atminstitucionalempresa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SobreEmpresaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sobre_empresa);
+    }
+}

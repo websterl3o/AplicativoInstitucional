f0618222760e167bd09bc75a09a0f67cd0c57d95
Leonardo Webster <leonardowebster15@gmail.com>
2018-06-19 09:47:15 -0300

    Adicionando View de Clientes

--- app/src/main/java/institucionalempresa/cursoandroid/com/atminstitucionalempresa/MainActivity.java
+++ app/src/main/java/institucionalempresa/cursoandroid/com/atminstitucionalempresa/MainActivity.java
@@ -37,5 +37,12 @@ public class MainActivity extends Activity {
                 startActivity(new Intent(MainActivity.this, SobreServicosActivity.class));
             }
         });
+
+        sobreClientes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SobreClientesActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/institucionalempresa/cursoandroid/com/atminstitucionalempresa/SobreClientesActivity.java
@@ -0,0 +1,14 @@
+package institucionalempresa.cursoandroid.com.atminstitucionalempresa;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SobreClientesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sobre_clientes);
+    }
+}
